270ad6e532877da1783203551d80ba77f28b22ab
Andy Galkin <dalamar132@mail.ru>
2025-04-08 08:45:38 +0300

    Форматирование

--- src/com/andrew/CountWordsUtil.java
+++ src/com/andrew/CountWordsUtil.java
@@ -3,9 +3,7 @@ package com.andrew;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,8 +12,9 @@ public final class CountWordsUtil {
     private static char[] convertToCharArray(Path path) throws IOException {
         StringBuilder result = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
-            while (reader.readLine() != null)
-            result.append(reader.readLine());
+            while (reader.readLine() != null){
+                result.append(reader.readLine());
+            }
         }
         return result.toString().toCharArray();
     }
